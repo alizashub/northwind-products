a728c5ae0948f1086f17c2bd1d35c66b425d5870
Aliza's <aliza.visuals@gmail.com>
2025-11-26 22:56:56 +0500

    ProductID, ProductName, UnitPrice, UnitsInStock printed.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,11 +5,19 @@ public class App {
         Connection connection;
         connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root", "yearup");
         Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM northwind.products");
+        ResultSet resultSet = statement.executeQuery("SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM products");
 
         while(resultSet.next()) {
-        String name = resultSet.getString("productName");
-            System.out.println(name);
+            int id = resultSet.getInt("ProductID");
+            String name = resultSet.getString("productName");
+            double price = resultSet.getDouble("UnitPrice");
+            int stock = resultSet.getInt("UnitsInStock");
+
+            System.out.println("Product ID: " + id);
+            System.out.println("Product Name: " + name);
+            System.out.println("Product Price: " + price);
+            System.out.println("Product Quantity In Stock: " + stock);
+            System.out.println("*****************************");
         }
 
         connection.close();
