af1727dc5c1686f6af01cf2f7eebc427a3fb0a4a
Aliza's <aliza.visuals@gmail.com>
2025-12-02 01:27:03 +0500

    using a try catch and try with resources

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,79 +1,48 @@
 import java.sql.*;
-import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root", "yearup");
-            ResultSet resultSet = null;
-            Scanner myScanner = new Scanner(System.in);
 
-            System.out.println("What do you like to do?");
-            System.out.println("1 - Display all products");
-            System.out.println("2 - Display all customers");
-            System.out.println("0 - Exit");
-            System.out.print("Please enter your option:");
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root", "yearup");
+             Statement statement = connection.createStatement();
+             ResultSet resultSetProducts = statement.executeQuery("SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM products");
+        ) {
+            System.out.println("Products");
+            while (resultSetProducts.next()) {
+                int id = resultSetProducts.getInt("ProductID");
+                String name = resultSetProducts.getString("productName");
+                double price = resultSetProducts.getDouble("UnitPrice");
+                int stock = resultSetProducts.getInt("UnitsInStock");
 
-            int userOption = myScanner.nextInt();
-            Statement statement = connection.createStatement();
-
-
-            switch (userOption) {
-                case 1 -> displayProducts(connection);
-                case 2 -> displayCustomers(connection);
-                case 0 -> System.out.println("Goodbye! You are not exiting the application.");
-                default -> System.out.println("You have made an invalid choice.");
+                System.out.println("Product ID: " + id);
+                System.out.println("Product Name: " + name);
+                System.out.println("Product Price: " + price);
+                System.out.println("Product Quantity In Stock: " + stock);
+                System.out.println("*****************************");
             }
 
+            ResultSet resultSetCustomer = statement.executeQuery("SELECT ContactName, CompanyName, City, Country, Phone " + "FROM customers ORDER BY Country"
+            );
+
+            System.out.println("Customers");
+            while (resultSetCustomer.next()) {
+                String contact = resultSetCustomer.getString("ContactName");
+                String company = resultSetCustomer.getString("CompanyName");
+                String city = resultSetCustomer.getString("City");
+                String country = resultSetCustomer.getString("Country");
+               String phone = resultSetCustomer.getString("Phone");
+
+                System.out.println("Contact : " + contact);
+                System.out.println("Company : " + company);
+                System.out.println("City    : " + city);
+                System.out.println("Country : " + country);
+                System.out.println("Phone   : " + phone);
+                System.out.println("*****************************");
+
+            }
         } catch (SQLException e) {
-            System.err.println("We have an error" + e);
+            System.out.println("Error" + e);
         }
     }
 
-    resultSet =statement.
-
-            executeQuery("SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM products");
-
-        while(resultSet.
-
-                next())
-
-    {
-        int id = resultSet.getInt("ProductID");
-        String name = resultSet.getString("productName");
-        double price = resultSet.getDouble("UnitPrice");
-        int stock = resultSet.getInt("UnitsInStock");
-
-        System.out.
-
-                println("Product ID: " + id);
-        System.out.
-
-                println("Product Name: " + name);
-        System.out.
-
-                println("Product Price: " + price);
-        System.out.
-
-                println("Product Quantity In Stock: " + stock);
-        System.out.
-
-                println("*****************************");
-    }
-}else if(userOption ==2)
-
-        {
-
-
-        }
-
-        }catch(
-SQLException e){
-        System.err.
-
-println("We have an error"+e);
-
-        }
-
-                }
-                }
+}
