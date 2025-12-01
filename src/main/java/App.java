cbb586156ddc637f1bac50536d75e303e083c57d
Aliza's <aliza.visuals@gmail.com>
2025-12-01 21:56:23 +0500

    using a try catch and try with resources

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,26 +1,29 @@
 import java.sql.*;
 
 public class App {
-    public static void main(String[] args) throws SQLException {
-        Connection connection;
-        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root", "yearup");
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM products");
+    public static void main(String[] args) {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root", "yearup");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM products");
 
-        while(resultSet.next()) {
-            int id = resultSet.getInt("ProductID");
-            String name = resultSet.getString("productName");
-            double price = resultSet.getDouble("UnitPrice");
-            int stock = resultSet.getInt("UnitsInStock");
+            while (resultSet.next()) {
+                int id = resultSet.getInt("ProductID");
+                String name = resultSet.getString("productName");
+                double price = resultSet.getDouble("UnitPrice");
+                int stock = resultSet.getInt("UnitsInStock");
 
-            System.out.println("Product ID: " + id);
-            System.out.println("Product Name: " + name);
-            System.out.println("Product Price: " + price);
-            System.out.println("Product Quantity In Stock: " + stock);
-            System.out.println("*****************************");
+                System.out.println("Product ID: " + id);
+                System.out.println("Product Name: " + name);
+                System.out.println("Product Price: " + price);
+                System.out.println("Product Quantity In Stock: " + stock);
+                System.out.println("*****************************");
+            }
+
+        } catch (SQLException e) {
+            System.err.println("We have an error" + e);
+
         }
-
-        connection.close();
 
     }
 }
